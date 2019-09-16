dcdbdef80b1bc8c82164bcdbb20a0dddee1c2878
Handle <mail@bysb.net>
2019-09-16 22:32:01 +0800

    增加无效输入判断

--- 20190915/CalcGUI.py
+++ 20190915/CalcGUI.py
@@ -2,34 +2,47 @@ import tkinter
 import CalculatorV2 as calc
 buttonSize = 64
 window = tkinter.Tk()
-window.geometry(""+str(buttonSize*4)+"x"+str(buttonSize*6))
+window.geometry(""+str(buttonSize*4)+"x"+str(buttonSize*6+buttonSize//2))
 window.resizable(False, False)
 window.title("计算器")
 inputLine = tkinter.Label(window, justify=tkinter.RIGHT,
-                          text='0', font="Helvetica 24", padx=8, anchor="e")
-inputLine.place(x=0, y=0, width=buttonSize*4, height=buttonSize)
+                          text='0', font="Helvetica 12", padx=8, anchor="e")
+inputLine.place(x=0, y=0, width=buttonSize*4, height=buttonSize/2)
+
+resultLine = tkinter.Label(window, justify=tkinter.RIGHT,
+                           text='0', font="Helvetica 24", padx=8, anchor="e")
+resultLine.place(x=0, y=buttonSize/2, width=buttonSize*4, height=buttonSize)
 
 
 def appendText(text: str):
-    print("Input:", text)
-    if text == "Del":
-        inputLine.config(text=inputLine["text"][:-1])
-        if len(inputLine["text"]) == 0:
-            inputLine.config(text="0")
+    # print("Input:", text)
+    if text == "AC":
+        # inputLine.config(text=inputLine["text"][:-1])
+        # if len(inputLine["text"]) == 0:
+        #     inputLine.config(text="0")
+        inputLine.config(text="0")
+        resultLine.config(text="0")
     elif text == "=":
-        inputLine.config(text=str(calc.calc(inputLine["text"])))
+        result: str
+        print("Calc", inputLine["text"])
+        try:
+            result = str(calc.calc(inputLine["text"]))
+        except calc.CalcError as err:
+            result = err.errorinfo
+        finally:
+            resultLine.config(text=result)
     elif inputLine["text"] == "0":
         inputLine.config(text=text)
     else:
         inputLine.config(text=inputLine["text"] + text)
-    print("Label", inputLine["text"])
+    # print("Label", inputLine["text"])
 
 
 buttons = [tkinter.Button(window, text=t) for t in [
-    '%', '^', 'Del', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '(', ')', '0', '=']]
+    '%', '^', 'AC', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '(', ')', '0', '=']]
 for index, btn in enumerate(buttons):
     btn.place(x=index % 4 * buttonSize, y=buttonSize + index //
-              4 * buttonSize, width=buttonSize, height=buttonSize)
+              4 * buttonSize + buttonSize/2, width=buttonSize, height=buttonSize)
     btn.bind("<Button-1>", lambda e: appendText(e.widget["text"]))
 # inputLine
 window.mainloop()
--- 20190915/CalculatorV2.py
+++ 20190915/CalculatorV2.py
@@ -1,4 +1,17 @@
 # 使用栈实现的计算器
+
+# 计算器错误
+
+
+class CalcError(Exception):
+    def __init__(self, ErrorInfo):
+        super().__init__(self)  # 初始化父类
+        self.errorinfo = ErrorInfo
+
+    def __str__(self):
+        return self.errorinfo
+
+
 def calc(formula: str) -> int:
     # 符号定义
     bracket_left_char = '('
@@ -24,6 +37,18 @@ def calc(formula: str) -> int:
     numbers_stack = []
     # 运算符 栈
     symbols_stack = []
+
+    # 将字符串转为int
+    def buf_to_int(buff: str) -> int:
+        num_flag = False
+        for num_char in num_chars:
+            if num_char in buff:
+                num_flag = True
+                break
+        if not num_flag:
+            raise CalcError("无效输入")
+        return int(buff)
+
     # 给定 a,b,符号 计算结果
 
     def __calc_a_b__(a: int, b: int, symbol: str) -> int:
@@ -35,6 +60,8 @@ def calc(formula: str) -> int:
         elif symbol == '*':
             return b * a
         elif symbol == '/':
+            if a == 0:
+                raise CalcError("除数不能为 0")
             return b // a
         elif symbol == '%':
             return b % a
@@ -52,6 +79,8 @@ def calc(formula: str) -> int:
             if bracket_count == 0:
                 # print("括号内容运算" + formula[bracketStart + 1: index])
                 numbers_stack.append(calc(formula[bracket_start + 1: index]))
+            elif bracket_count < 0:
+                raise CalcError("括号不匹配")
         elif bracket_count == 0:
             # 如果为数字
             if char in num_chars:
@@ -59,14 +88,14 @@ def calc(formula: str) -> int:
             elif char in symbols:
                 # 将数字入栈
                 if len(number_buffer):
-                    numbers_stack.append(int(number_buffer))
+                    numbers_stack.append(buf_to_int(number_buffer))
                     number_buffer = ''
                 # 负号单独处理
                 if char == '-':
                     if index == 0 or formula[index - 1] in symbols or formula[index - 1] == bracket_right_char:
                         number_buffer += char
                 # 如果符号运算优先级低于栈，则将栈清理到和当前运算级相等为止
-                while len(symbols_stack) and symbols_stack[len(symbols_stack)-1]["level"] > priority_level[char]:
+                while len(symbols_stack) and symbols_stack[len(symbols_stack)-1]["level"] > priority_level[char] and len(numbers_stack) > 1:
                     numbers_stack.append(__calc_a_b__(
                         numbers_stack.pop(), numbers_stack.pop(), symbols_stack.pop()["symbol"]))
                 symbols_stack.append({
@@ -76,12 +105,17 @@ def calc(formula: str) -> int:
     # 递归结束后，按照栈直接进行计算
     # 先检测是否有数字未入栈
     if len(number_buffer):
-        numbers_stack.append(int(number_buffer))
+        numbers_stack.append(buf_to_int(number_buffer))
     # 进行计算
-    while len(symbols_stack):
+    while len(symbols_stack) and len(numbers_stack) > 1:
         numbers_stack.append(__calc_a_b__(
             numbers_stack.pop(), numbers_stack.pop(), symbols_stack.pop()["symbol"]))
-    return numbers_stack.pop()
+    if len(numbers_stack) > 1 or len(symbols_stack):
+        raise CalcError("无效输入")
+    if len(numbers_stack):
+        return numbers_stack.pop()
+    else:
+        0
 
 
 # while True:
